9c9fbb8d579812d352ea64c3939449e8f54aad9b
Aroon Narayanan <aroonnarayanan@gmail.com>
2016-12-08 15:03:58 -0800

    added pdf generator

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
-﻿var express = require('express')
-var bodyParser = require('body-parser')
-var multer = require('multer')
+﻿var express = require('express');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var htmlPdf = require('html-pdf');
+
+var app = express();
+var router = express.Router();
+
+
+
+
+
+
+
+
+var port = process.env.port || 80;
+app.listen(port);
+console.log('Server ready on port ' + port);
